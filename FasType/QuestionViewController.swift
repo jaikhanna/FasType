2932c5afa63e0b204b4075b480b4b514eee2a1fa
jaikhanna <jkhanna23@gmail.com>
2016-02-26 00:36:29 -0800

    starting project again

--- FasType/QuestionViewController.swift
+++ FasType/QuestionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuestionViewController.swift
 //  FasType
 //
 //  Created by Jai Khanna on 11/14/15.
@@ -8,36 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuestionViewController: UIViewController {
     
-    var sentences = ["hello world", "kya haal hai?"]
+    var sentences = ["hello world", "HackSC", "Two Sigma","bonjour", "Apple", "Microsoft"]
        
+    @IBOutlet weak var questionString: UILabel!
     @IBOutlet weak var userInput: UITextField!
+    
+    @IBOutlet var countingLabel: UILabel!
+    var timer = NSTimer()
+    var counter = 0
     
     
     
     @IBAction func submitResponse(sender: UIButton) {
         
-        
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        [userInput .becomeFirstResponder()]
         
+        countingLabel.text = String(counter)
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
         
+        self.navigationItem.setHidesBackButton(true, animated:true);
+        var randomIndex = Int(arc4random_uniform(UInt32(sentences.count)))
         
-        let label = UILabel(frame: CGRectMake(0, 0, 100, 21))
-        label.center = CGPointMake(160, 284)
-        label.textAlignment = NSTextAlignment.Center
-        label.text = sentences[0]
-        self.view.addSubview(label)
-        
+        if(randomIndex>0)
+        {
+            randomIndex -= 1
+        }
+        questionString.text = sentences[randomIndex]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+   
+    override func shouldPerformSegueWithIdentifier(identifier: String!, sender: AnyObject!) -> Bool {
+        if identifier == "inputToScore" {
+            
+            if (userInput.text!.isEmpty) {
+                
+                let alert = UIAlertView()
+                alert.title = "Oops"
+                alert.message = "I think you forgot to type"
+                alert.addButtonWithTitle("Start typing!")
+                alert.show()
+                
+                return false
+            }
+                
+            else {
+                return true
+            }
+        }
+        
+        // by default, transition
+        return true
+    }
+    
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+            if segue.identifier == "inputToScore"{
+                
+                timer.invalidate()
+                let svc = segue.destinationViewController as! ScoreViewController
+                svc.answer = userInput.text
+                svc.question = questionString.text
+                svc.totalTime = countingLabel.text
+            }
+    }
+    
+    func updateCounter(){
+        countingLabel.text = String(counter++)
     }
 
 }
--- FasType/ScoreViewController.swift
+++ FasType/ScoreViewController.swift
@@ -7,3 +7,112 @@
 //
 
 import Foundation
+import UIKit
+
+class ScoreViewController: UIViewController {
+    
+   
+    @IBOutlet weak var userScore: UILabel!
+    @IBOutlet weak var misMatch: UILabel!
+    @IBOutlet weak var timeTaken: UILabel!
+    
+    var answer: String!
+    var question: String!
+    var levScore: Double!
+    var finalScore: Double!
+    var questionLen: Double!
+    var totalTime: String!
+    
+    
+    
+    
+    func min(numbers: Int...) -> Int {
+        return numbers.reduce(numbers[0], combine: {$0 < $1 ? $0 : $1})
+    }
+    
+    class Array2D {
+        var cols:Int, rows:Int
+        var matrix: [Int]
+        
+        
+        init(cols:Int, rows:Int) {
+            self.cols = cols
+            self.rows = rows
+            matrix = Array(count:cols*rows, repeatedValue:0)
+        }
+        
+        subscript(col:Int, row:Int) -> Int {
+            get {
+                return matrix[cols * row + col]
+            }
+            set {
+                matrix[cols*row+col] = newValue
+            }
+        }
+        
+        func colCount() -> Int {
+            return self.cols
+        }
+        
+        func rowCount() -> Int {
+            return self.rows
+        }
+    }
+    
+    
+    func levenshtein(aStr: String, bStr: String) -> Int {
+        let a = Array(aStr.utf16)
+        let b = Array(bStr.utf16)
+        
+        var dist = Array2D(cols: a.count + 1, rows: b.count + 1)
+        
+        for i in 1...a.count {
+            dist[i, 0] = i
+        }
+        
+        for j in 1...b.count {
+            dist[0, j] = j
+        }
+        
+        for i in 1...a.count {
+            for j in 1...b.count {
+                if a[i-1] == b[j-1] {
+                    dist[i, j] = dist[i-1, j-1]  // noop
+                } else {
+                    dist[i, j] = min(
+                        dist[i-1, j] + 1,  // deletion
+                        dist[i, j-1] + 1,  // insertion
+                        dist[i-1, j-1] + 1  // substitution
+                    )
+                }
+            }
+        }
+        
+        return dist[a.count, b.count]
+    }
+    
+    override func viewDidLoad() {
+        self.navigationItem.setHidesBackButton(true, animated:true);
+        
+        if answer == ""{
+            
+        }else{
+            questionLen = Double(question.characters.count)
+            levScore = Double(levenshtein(answer,bStr: question))
+            
+            finalScore = round(((questionLen - levScore)/questionLen) * 100)
+            userScore.text = "Your score is: \(Int(finalScore)) %"
+            misMatch.text = "Mismatch character(s): \(Int(levScore))"
+            timeTaken.text = "You took \(totalTime) seconds"
+            
+        }
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        if segue.identifier == "resetGame"{
+            let qvc = segue.destinationViewController as! QuestionViewController
+        }
+    }
+    
+}
